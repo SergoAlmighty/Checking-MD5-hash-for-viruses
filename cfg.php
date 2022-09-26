e558d8b7899fdd00b191d9eecbf9ff20dafa046f
Sergey Chernyshevich <fan1235@gmail.com>
2022-09-26 14:59:37 +0300

    Created 2 file: .gitignore and cfg.php

---
+++ cfg.php
@@ -0,0 +1,16 @@
+<?php
+
+// database record to be exported
+$db_record = 'user_info';
+// optional where query
+$where = 'md5';
+// filename for export
+$txt_filename = 'scanned-files/data_md5'.'.txt';
+// database variables
+$hostname = "localhost";
+$user = "user_login";
+$password = "passowrd";
+$database = "name_db";
+$port = 3306;
+
+?>
