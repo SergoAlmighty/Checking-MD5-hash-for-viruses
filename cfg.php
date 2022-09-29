033e70038393e807b411b8bfd76996f576326f16
Sergey Chernyshevich <fan1235@gmail.com>
2022-09-29 22:39:31 +0300

    Loaded configuration file

---
+++ cfg.php
@@ -0,0 +1,18 @@
+<?php
+
+// database record to be exported
+$db_record = 'user_info';
+// optional where query
+$where = 'md5';
+// filename for export
+$txt_filename = 'scanned-files/data_md5'.'.txt';
+// database variables
+$hostname = "localhost";
+
+$user = "user_login";
+$password = "user_passowrd";
+$database = "name_db";
+
+$port = 3306;
+
+?>
